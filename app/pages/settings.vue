a610b8d763eb4f52b488b980a004e6705d0a781e
Przemek Macznik <przemyslaw.macznik@codeconcept.pl>
2025-12-09 09:43:39 +0100

    settings update

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -5,11 +5,11 @@ import type { Settings } from '~/composables/settings/settingsTypes';
 import CustomSelect from '~/components/ui-elements/CustomSelect.vue';
 
 const uSettingsFetch = useSettingsFetch()
-const {data: settings} = await uSettingsFetch.getSettings()
+const {data: settings, error, status} = await uSettingsFetch.getSettings()
 
 
 const settingsBody = ref<Settings>()
-const {error, execute} = uSettingsFetch.updateSettings(settingsBody)
+const {execute} = uSettingsFetch.updateSettings(settingsBody)
 
 const changeWeekStart = (event?: number) => {
   if (event) {
@@ -17,28 +17,35 @@ const changeWeekStart = (event?: number) => {
     execute()
   }
 }
-const weekStart = computed(() => settings.value?.weekStart ?? 1)
-
 </script>
 
 <template>
   <div>
-    <h1>Settings</h1>
-    <div class="setting-item">
-      <label for="week-start">Week Start:</label>
-      {{ settings?.weekStart }}
-      <CustomSelect
-        :model-value="settings?.weekStart"
-        :options="[
-          {value: 0, label: 'Sunday'},
-          {value: 1, label: 'Monday'},
-          {value: 6, label: 'Saturday'},
-        ]"
-        @update:model-value="changeWeekStart"
-      />
+    <div v-if="error">
+      Error
     </div>
-    <div class="current-value">
-      Current value: {{ settings?.weekStart }}
+
+    <div v-else-if="status === 'pending'">
+      Fetching
+    </div>
+
+    <div v-else-if="settings">
+      <h1>Settings</h1>
+      <div class="setting-item">
+        <label for="week-start">Week Start:</label>
+        <CustomSelect
+          :model-value="settings?.weekStart"
+          :options="[
+            {value: 0, label: 'Sunday'},
+            {value: 1, label: 'Monday'},
+            {value: 6, label: 'Saturday'},
+          ]"
+          @update:model-value="changeWeekStart($event as number)"
+        />
+      </div>
+      <div class="current-value">
+        Current value: {{ settings?.weekStart }}
+      </div>
     </div>
   </div>
 </template>
